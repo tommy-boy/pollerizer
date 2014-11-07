fed835a84375d227f7431954d89b7babc59f145e
Ryan Konig <ryan.konig@gmail.com>
2014-11-06 22:17:18 -0700

    Successfully casts vote on Geoblender db table

--- app/src/main/java/com/geoblender/pollerizer/MainActivity.java
+++ app/src/main/java/com/geoblender/pollerizer/MainActivity.java
@@ -9,7 +9,22 @@ import android.widget.Button;
 import android.widget.Toast;
 import android.widget.RelativeLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends Activity {
+
+    private static final String SERVICE_URL = "http://geoblender.com/android/android.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,8 @@ public class MainActivity extends Activity {
 
             int counter = 0;
             public void onClick(View view) {
+
+                setUpMap();
 
                 counter ++;
 
@@ -52,5 +69,48 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void retrieveAndAddCities() throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            // Connect to the web service
+            URL url = new URL(SERVICE_URL);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "No connection",
+                    Toast.LENGTH_SHORT).show();
+            //Log.e(LOG_TAG, "Error connecting to service", e);
+            throw new IOException("Error connecting to service", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    } // connect to web
+
+    private void setUpMap() {
+        // Retrieve the city data from the web service
+        // In a worker thread since it's a network operation.
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    retrieveAndAddCities();
+                } catch (IOException e) {
+                    //Log.e(LOG_TAG, "Cannot retrive cities", e);
+                    return;
+                }
+            }
+        }).start();
     }
 }

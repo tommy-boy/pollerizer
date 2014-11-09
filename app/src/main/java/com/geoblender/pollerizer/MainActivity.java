efc28229d0ee4aa91f92ddf8a90da3aa76207e20
Ryan Konig <ryan.konig@gmail.com>
2014-11-09 09:20:00 -0700

    Looping JSON

--- app/src/main/java/com/geoblender/pollerizer/MainActivity.java
+++ app/src/main/java/com/geoblender/pollerizer/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.RelativeLayout;
 
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -21,6 +25,8 @@ public class MainActivity extends Activity {
     private static String SERVICE_URL = "http://geoblender.com/android/android.php";
     private static final String SERVICE_URL_Y = "http://geoblender.com/android/yes.php";
     private static final String SERVICE_URL_N = "http://geoblender.com/android/no.php";
+    // 1. Declare a service url:
+    private static final String SERVICE_URL_SURVEYS = "http://www.geoblender.com/a/surveys.json";
 
     private static final String LOG_TAG = "Pollerizer";
 
@@ -32,6 +38,8 @@ public class MainActivity extends Activity {
         final Button noButton = (Button) findViewById(R.id.buttonNo);
 
         final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        getData();
 
         View.OnClickListener yes = new View.OnClickListener() {
             int counter = 0;
@@ -57,7 +65,7 @@ public class MainActivity extends Activity {
 
         yesButton.setOnClickListener(yes);
         noButton.setOnClickListener(no);
-    }
+    } // onCreate
 
 
     @Override
@@ -129,5 +137,82 @@ public class MainActivity extends Activity {
                 }
             }
         }).start();
+    }
+
+    // 2. Create function to call the function to retrieve JSON:
+    private void getData() {
+        // Retrieve the city data from the web service
+        // In a worker thread since it's a network operation.
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    retrieveData();
+                    Log.e(LOG_TAG, "Data retrived");
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Cannot retrive data", e);
+                    return;
+                }
+            }
+        }).start();
+    }
+
+    // 3. Create function to retrieve JSON:
+    protected void retrieveData() throws IOException {
+        HttpURLConnection conn = null;
+        final StringBuilder json = new StringBuilder();
+        try {
+            // Connect to the web service
+            URL url = new URL(SERVICE_URL_SURVEYS);
+            conn = (HttpURLConnection) url.openConnection();
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+            // Read the JSON data into the StringBuilder
+            int read;
+            char[] buff = new char[1024];
+            while ((read = in.read(buff)) != -1) {
+                json.append(buff, 0, read);
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error connecting to service", e);
+            throw new IOException("Error connecting to service", e);
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        // Do stuff with JSON
+        // Must run this on the UI thread since it's a UI operation.
+        runOnUiThread(new Runnable() {
+            public void run() {
+                try {
+                    processJson(json.toString());
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "Error processing JSON", e);
+                }
+            }
+        });
+    }
+
+    // 4. Create function to process JSON
+    void processJson(String json) throws JSONException {
+        // De-serialize the JSON string into an array of objects
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            Log.e(LOG_TAG, "Looping");
+
+            JSONObject jsonObj = jsonArray.getJSONObject(i);
+            /*map.addMarker(new MarkerOptions()
+                            .title(jsonObj.getString("name"))
+                            .snippet(jsonObj.getString("description"))
+                            .position(new LatLng(
+                                    Double.parseDouble(jsonObj.getString("latitude")),
+                                    Double.parseDouble(jsonObj.getString("longitude"))
+                            ))
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.yellow_point))
+
+            );*/
+        } // loop
     }
 }
